203210aa1ab461f71264803c9715cdfd8820351c
patiskiba <patricia.skiba@mscoding.org>
2025-04-28 11:59:45 -0500

    Start

---
+++ app.js
@@ -0,0 +1,12 @@
+//! PART 1: TOTAL VALUES OF COINS
+
+//? Coin type values
+let pennyValue = 0.01;
+let nickelValue = 0.05;
+let dimeValue = 0.10;
+let quarterValue = 0.25;
+
+
+//? Coin counts
+let pennyCount = 200;
+let nickelCount = 200;
