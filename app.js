304f2982374521579c17ae160b32f10d3f6f848e
patiskiba <patricia.skiba@mscoding.org>
2025-04-28 13:51:12 -0500

    Part 1 done

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ let dimeValue = 0.10;
 let quarterValue = 0.25;
 
 
-//? Coin counts
+//? Coin counts (PLACEHOLDERS)
 let pennyCount = 200;
 let nickelCount = 200;
+let dimeCount = 200;
+let quarterCount = 200;
+
+//? Calculate totals
+let pennyValueTotal = pennyValue * pennyCount;
+let nickelValueTotal = nickelValue * nickelCount;
+let dimeValueTotal = dimeValue * dimeCount;
+let quarterValueTotal = quarterValue * quarterCount;
+let combinedValueTotal = pennyValueTotal + nickelValueTotal + dimeValueTotal + quarterValueTotal;
+
+console.log(`Pennies: $${pennyValueTotal}`);
+console.log(`Nickels: $${nickelValueTotal}`);
+console.log(`Dimes: $${dimeValueTotal}`);
+console.log(`Quarters: $${quarterValueTotal}`);
+console.log(`Combined Total: $${combinedValueTotal}`);
